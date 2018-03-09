e74545561e9b412d689444e99cfa09701755111b
Kaishen Wang <kaishenwang@Kaishens-MacBook-Pro.local>
2018-03-09 15:27:01 -0600

    generate full logs for config

---
+++ ctsync-pull/generateConfig.py
@@ -0,0 +1,19 @@
+import json
+from collections import OrderedDict
+readFileName = "all_logs_list.json"
+writeFileName = "fullConfig.json"
+
+readData = json.load(open(readFileName))
+logs = readData["logs"]
+f= open(writeFileName,"w")
+f.close()
+
+for log in logs:
+	writeData = OrderedDict()
+	nameString = log["description"]
+	writeData["name"] = "CT_SERVER_"+nameString.upper().replace(" ","_").replace("'", "")
+	writeData["url"] = log["url"]
+	writeData["batch_size"] = 1000
+	with open(writeFileName, 'a') as outfile:
+		json.dump(writeData, outfile)
+		outfile.write("\n")
