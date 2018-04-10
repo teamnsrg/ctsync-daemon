15185a847aafed01679126a53ce1a083901927b5
kwang40 <kaishenw1995@gmail.com>
2018-04-10 12:52:52 -0500

    enable multiple threads

--- ctsync-pull/x509Parser.cpp
+++ ctsync-pull/x509Parser.cpp
@@ -1,4 +1,4 @@
-// g++ x509Parser.cpp -std=c++14 -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include -o x509Parser
+//g++ Queue.h x509Parser.cpp -std=c++14 -lpthread -o x509Parser -I /usr/local/ssl/include -L /usr/local/ssl/lib -lssl -lcrypto -Wall
 // cite from: http://www.zedwood.com/article/c-openssl-parse-x509-certificate-pem
 #include <openssl/bio.h>
 #include <openssl/x509.h>
@@ -8,9 +8,13 @@
 #include <fstream>
 #include <streambuf>
 #include <iostream>
+#include "Queue.h"
+#include <thread>
+#include <atomic>
 
 using namespace std;
-
+atomic<bool> NOT_FINISHED (true);
+const int CERTS_NUM = 100000;
 
 X509* getCert(string &cert_str)
 {
@@ -22,55 +26,59 @@ X509* getCert(string &cert_str)
     return cert;
 }
 
-void parseCert2(X509* x509)
+void parseCert2(Queue<X509*>& q)
 {
-    cout <<"--------------------" << endl;
-    BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
- 
-    long l = X509_get_version(x509);
-    BIO_printf(bio_out, "Version: %ld\n", l+1);
- 
-    ASN1_INTEGER *bs = X509_get_serialNumber(x509);
-    BIO_printf(bio_out,"Serial: ");
-    for(int i=0; i<bs->length; i++) {
-        BIO_printf(bio_out,"%02x",bs->data[i] );
-    }
-    BIO_printf(bio_out,"\n");
- 
-    X509_signature_print(bio_out, x509->sig_alg, NULL);
- 
-    BIO_printf(bio_out,"Issuer: ");
-    X509_NAME_print(bio_out,X509_get_issuer_name(x509),0);
-    BIO_printf(bio_out,"\n");
- 
-    BIO_printf(bio_out,"Valid From: ");
-    ASN1_TIME_print(bio_out,X509_get_notBefore(x509));
-    BIO_printf(bio_out,"\n");
- 
-    BIO_printf(bio_out,"Valid Until: ");
-    ASN1_TIME_print(bio_out,X509_get_notAfter(x509));
-    BIO_printf(bio_out,"\n");
- 
-    BIO_printf(bio_out,"Subject: ");
-    X509_NAME_print(bio_out,X509_get_subject_name(x509),0);
-    BIO_printf(bio_out,"\n");
- 
-    EVP_PKEY *pkey=X509_get_pubkey(x509);
-    EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
-    EVP_PKEY_free(pkey);
- 
-    X509_CINF *ci=x509->cert_info;
-    X509V3_extensions_print(bio_out, (char*)"X509v3 extensions", ci->extensions, X509_FLAG_COMPAT, 0);
- 
-    X509_signature_print(bio_out, x509->sig_alg, x509->signature);
-    BIO_free(bio_out);
-}
+    int count = 0;
+    while (NOT_FINISHED || !q.empty()) {
+	count += 1;
+        X509* x509 = q.pop();
+        cout <<"--------------------" << endl;
+        BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE); 
 
-void printCert(string filename) {
-    ifstream infile(filename);
+        long l = X509_get_version(x509);
+        BIO_printf(bio_out, "Version: %ld\n", l+1); 
+
+        ASN1_INTEGER *bs = X509_get_serialNumber(x509);
+        BIO_printf(bio_out,"Serial: ");
+        for(int i=0; i<bs->length; i++) {
+            BIO_printf(bio_out,"%02x",bs->data[i] );
+        }
+        BIO_printf(bio_out,"\n");   
+
+        X509_signature_print(bio_out, x509->sig_alg, NULL); 
+
+        BIO_printf(bio_out,"Issuer: ");
+        X509_NAME_print(bio_out,X509_get_issuer_name(x509),0);
+        BIO_printf(bio_out,"\n");   
+
+        BIO_printf(bio_out,"Valid From: ");
+        ASN1_TIME_print(bio_out,X509_get_notBefore(x509));
+        BIO_printf(bio_out,"\n");   
+
+        BIO_printf(bio_out,"Valid Until: ");
+        ASN1_TIME_print(bio_out,X509_get_notAfter(x509));
+        BIO_printf(bio_out,"\n");   
+
+        BIO_printf(bio_out,"Subject: ");
+        X509_NAME_print(bio_out,X509_get_subject_name(x509),0);
+        BIO_printf(bio_out,"\n");   
+
+        EVP_PKEY *pkey=X509_get_pubkey(x509);
+        EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
+        EVP_PKEY_free(pkey);    
+
+        X509_CINF *ci=x509->cert_info;
+        X509V3_extensions_print(bio_out, (char*)"X509v3 extensions", ci->extensions, X509_FLAG_COMPAT, 0);  
+
+        X509_signature_print(bio_out, x509->sig_alg, x509->signature);
+        BIO_free(bio_out);
+    }
+}
+void produceCerts(Queue<X509*>& q) {
+    ifstream infile("/data3/ct_data/log.txt");
     std::string line;
     int count = 0, error_count = 0, precert_count = 0;
-    while (count < 10000 && std::getline(infile, line)) {
+    while (count < CERTS_NUM && getline(infile, line)) {
         if (line.empty()) continue;
         count += 1;
 	// find cert string
@@ -93,7 +101,7 @@ void printCert(string filename) {
         string cert_str = "-----BEGIN CERTIFICATE-----\n" + line.substr(start_pos, end_pos-start_pos) + "\n-----END CERTIFICATE-----\n";
 	X509* c = getCert(cert_str);
 	if (ASN1_INTEGER_get(c->cert_info->version) != 0) {
-		parseCert2(c);
+		q.push(c);
 	} else {
 		//cerr << "Invalid Cert: \n" << cert_str << endl;
 		error_count += 1;
@@ -102,10 +110,15 @@ void printCert(string filename) {
     }
     cerr << "Error count" << error_count << endl;
     cerr << "Precert count" << precert_count << endl;
+    NOT_FINISHED = false;
 }
 
 int  main() {
-  OpenSSL_add_all_algorithms();
-  printCert("/data3/ct_data/log.txt");
-  return 0;
+    OpenSSL_add_all_algorithms();
+    Queue<X509*> q;
+    thread produceThread(bind(produceCerts, ref(q)));
+    thread comsumeThread(bind(&parseCert2, ref(q)));
+    produceThread.join();
+    comsumeThread.join();
+    return 0;
 }
