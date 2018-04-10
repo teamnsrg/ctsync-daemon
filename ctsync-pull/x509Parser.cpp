254e2e9be3f1ea2ec28cef59db8b9905d22b5abf
kwang40 <kaishenw1995@gmail.com>
2018-04-09 22:41:22 -0500

    able to get most of the cert parsed

--- ctsync-pull/x509Parser.cpp
+++ ctsync-pull/x509Parser.cpp
@@ -9,7 +9,6 @@
 #include <streambuf>
 #include <iostream>
 
-
 using namespace std;
 
 
@@ -23,54 +22,46 @@ X509* getCert(string &cert_str)
     return cert;
 }
 
-bool check_certificate_validaty(X509* cert)
-{
-
-ASN1_INTEGER *serial = X509_get_serialNumber(cert);
-
-BIGNUM *bn = ASN1_INTEGER_to_BN(serial, NULL);
-	return bn != nullptr;
-}
 void parseCert2(X509* x509)
 {
     cout <<"--------------------" << endl;
     BIO *bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
-
+ 
     long l = X509_get_version(x509);
     BIO_printf(bio_out, "Version: %ld\n", l+1);
-
+ 
     ASN1_INTEGER *bs = X509_get_serialNumber(x509);
     BIO_printf(bio_out,"Serial: ");
     for(int i=0; i<bs->length; i++) {
         BIO_printf(bio_out,"%02x",bs->data[i] );
     }
     BIO_printf(bio_out,"\n");
-
+ 
     X509_signature_print(bio_out, x509->sig_alg, NULL);
-
+ 
     BIO_printf(bio_out,"Issuer: ");
     X509_NAME_print(bio_out,X509_get_issuer_name(x509),0);
     BIO_printf(bio_out,"\n");
-
+ 
     BIO_printf(bio_out,"Valid From: ");
     ASN1_TIME_print(bio_out,X509_get_notBefore(x509));
     BIO_printf(bio_out,"\n");
-
+ 
     BIO_printf(bio_out,"Valid Until: ");
     ASN1_TIME_print(bio_out,X509_get_notAfter(x509));
     BIO_printf(bio_out,"\n");
-
+ 
     BIO_printf(bio_out,"Subject: ");
     X509_NAME_print(bio_out,X509_get_subject_name(x509),0);
     BIO_printf(bio_out,"\n");
-
+ 
     EVP_PKEY *pkey=X509_get_pubkey(x509);
     EVP_PKEY_print_public(bio_out, pkey, 0, NULL);
     EVP_PKEY_free(pkey);
-
+ 
     X509_CINF *ci=x509->cert_info;
     X509V3_extensions_print(bio_out, (char*)"X509v3 extensions", ci->extensions, X509_FLAG_COMPAT, 0);
-
+ 
     X509_signature_print(bio_out, x509->sig_alg, x509->signature);
     BIO_free(bio_out);
 }
@@ -78,17 +69,20 @@ void parseCert2(X509* x509)
 void printCert(string filename) {
     ifstream infile(filename);
     std::string line;
-    int count = 0, error_count = 0;
+    int count = 0, error_count = 0, precert_count = 0;
     while (count < 10000 && std::getline(infile, line)) {
         if (line.empty()) continue;
         count += 1;
+	if (count == 66)	continue;
 	// find cert string
+	bool precert = true;
         int comma_count = 0, start_pos = 0, end_pos = 0;
         for (int i = 0, len = line.size(); i < len; ++i) {
             if (start_pos == 0 && (comma_count == 5 || comma_count == 7))    start_pos = i;
             if (end_pos == 0 && (comma_count == 6 || comma_count == 8))    end_pos = i-1;
             if (start_pos > 0 && end_pos > 0) {
                 if (start_pos + 1 < end_pos) {
+		    if (comma_count == 6)	precert = false;
                     break;
                 } else {
                     start_pos = 0;
@@ -98,16 +92,17 @@ void printCert(string filename) {
             if (line[i] == ',') comma_count += 1;
         }
         string cert_str = "-----BEGIN CERTIFICATE-----\n" + line.substr(start_pos, end_pos-start_pos) + "\n-----END CERTIFICATE-----\n";
-	//cerr << cert_str.size() << endl;
 	X509* c = getCert(cert_str);
-	if (cert_str.size() > 1500) {
+	if (ASN1_INTEGER_get(c->cert_info->version) != 0) {
 		parseCert2(c);
 	} else {
 		//cerr << "Invalid Cert: \n" << cert_str << endl;
 		error_count += 1;
-        }
-        cerr << error_count << endl;
+	}
+	if (precert)  precert_count += 1;
     }
+    cerr << "Error count" << error_count << endl;
+    cerr << "Precert count" << precert_count << endl;
 }
 
 int  main() {
